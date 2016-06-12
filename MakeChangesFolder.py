53b9e6dee68d219eb512d12999fafbd983290a05
thurstonemerson <jmt.emerson@gmail.com>
2016-06-13 10:12:07 +1200

    Made script more generic

--- MakeChangesFolder.py
+++ MakeChangesFolder.py
@@ -8,24 +8,28 @@ import os
 
 
 
+
 if __name__ == '__main__':
+         
+    package_name = "\\com\\treasures"    
+    commit_message = "Committing file "
          
     x = 0     
          
     for file in os.listdir("changes"):
         if file.endswith(".java"):
-            algorithm, file_extension = os.path.splitext(file)
-            print (algorithm)
+            file_name, file_extension = os.path.splitext(file)
+            print (file_name)
             
             changedir = "changes\\" + str(x)
-            epidir = "changes\\" + str(x) + "\\com\\epi"
-            os.makedirs(epidir)
-            print ("created dir {0}".format(epidir))
+            packagedir = "changes\\" + str(x) + package_name
+            os.makedirs(packagedir)
+            print ("created dir {0}".format(packagedir))
     
             with open(changedir + '\\commit-message.txt', 'w') as f:
-                f.write('Algorithm ' + algorithm + ' from Elements of Programming Interviews, for comparison purposes only')
+                f.write(commit_message + file_name)
     
-            os.system('cp %s %s' % ("changes\\" + file, epidir + "\\" + file))
+            os.system('cp %s %s' % ("changes\\" + file, packagedir + "\\" + file))
     
             x=x+1
             
